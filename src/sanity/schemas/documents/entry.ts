eda0f6e819126c48150eb9abea6b35610060f11d
Mason Mathai <masonmathai@gmail.com>
2024-08-28 17:35:38 -0400

    updated gitignore

--- src/sanity/schemas/documents/entry.ts
+++ src/sanity/schemas/documents/entry.ts
@@ -1,74 +1,73 @@
-import { ColorWheelIcon } from '@sanity/icons'
-import { defineArrayMember, defineField, defineType } from 'sanity'
+import { ColorWheelIcon } from "@sanity/icons";
+import { defineArrayMember, defineField, defineType } from "sanity";
 
 export default defineType({
-  name: 'entry',
-  title: 'Entry',
+  name: "entry",
+  title: "Entry",
   icon: ColorWheelIcon,
-  type: 'document',
+  type: "document",
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
       validation: (rule) => rule.max(25).required(),
     }),
     defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 96,
         isUnique: (value, context) => context.defaultIsUnique(value, context),
       },
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: 'category',
-      title: 'Category',
-      type: 'reference',
-      to: [{ type: 'category' }],
+      name: "category",
+      title: "Category",
+      type: "reference",
+      to: [{ type: "category" }],
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: 'tags',
-      title: 'Tags',
+      name: "tags",
+      title: "Tags",
       description:
-        'Include any related tags for the entry, to be used for filtering',
-      type: 'array',
+        "Include any related tags for the entry, to be used for filtering",
+      type: "array",
       of: [
         defineArrayMember({
-          type: 'reference',
-          to: [{ type: 'tag' }],
+          type: "reference",
+          to: [{ type: "tag" }],
         }),
       ],
       options: {
-        layout: 'tags',
+        layout: "tags",
       },
     }),
     defineField({
-      name: 'shortDescription',
-      title: 'Short Description',
-      type: 'string',
-      validation: (rule) => rule.required(),
+      name: "shortDescription",
+      title: "Short Description",
+      type: "string",
     }),
     defineField({
-      name: 'medium',
-      title: 'Medium',
-      type: 'reference',
-      to: [{ type: 'medium' }],
+      name: "medium",
+      title: "Medium",
+      type: "reference",
+      to: [{ type: "medium" }],
     }),
     defineField({
-      name: 'size',
-      title: 'Size',
-      type: 'string',
+      name: "size",
+      title: "Size",
+      type: "string",
     }),
     defineField({
-      name: 'content',
-      description: 'Any text content for the entry.',
-      title: 'Content',
-      type: 'array',
+      name: "content",
+      description: "Any text content for the entry.",
+      title: "Content",
+      type: "array",
       of: [
         // Paragraphs
         defineArrayMember({
@@ -77,56 +76,56 @@ export default defineType({
             annotations: [],
             decorators: [
               {
-                title: 'Italic',
-                value: 'em',
+                title: "Italic",
+                value: "em",
               },
               {
-                title: 'Strong',
-                value: 'strong',
+                title: "Strong",
+                value: "strong",
               },
             ],
           },
           styles: [],
-          type: 'block',
+          type: "block",
         }),
       ],
     }),
     defineField({
-      name: 'image',
-      title: 'Image',
-      type: 'image',
+      name: "image",
+      title: "Image",
+      type: "image",
     }),
     defineField({
-      name: 'orientation',
-      title: 'Orientation',
-      type: 'reference',
-      to: [{ type: 'orientation' }],
+      name: "orientation",
+      title: "Orientation",
+      type: "reference",
+      to: [{ type: "orientation" }],
     }),
     defineField({
-      name: 'secondaryImage',
-      title: 'Secondary Image',
-      type: 'image',
+      name: "secondaryImage",
+      title: "Secondary Image",
+      type: "image",
     }),
     defineField({
-      name: 'location',
-      title: 'Location',
-      type: 'string',
+      name: "location",
+      title: "Location",
+      type: "string",
     }),
     defineField({
-      name: 'date',
-      title: 'Date',
-      type: 'string',
+      name: "date",
+      title: "Date",
+      type: "string",
       validation: (rule) => rule.max(10),
     }),
   ],
   preview: {
     select: {
-      title: 'title',
-      date: 'date',
-      media: 'image',
+      title: "title",
+      date: "date",
+      media: "image",
     },
     prepare({ title, media, date }) {
-      return { title, media, subtitle: date }
+      return { title, media, subtitle: date };
     },
   },
-})
+});
